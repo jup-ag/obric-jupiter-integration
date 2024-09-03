e8c2e94a22deba16bdf03a285f3634feeb339b5b
kiv1 <zaki.jst@gmail.com>
2024-09-03 14:32:02 +0800

    Bump pyth crate

--- rust-integration/accounts/programs/obric-solana/src/state/price_feed.rs
+++ rust-integration/accounts/programs/obric-solana/src/state/price_feed.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 use pyth_sdk::{Price, UnixTimestamp};
-use pyth_sdk_solana::state::load_price_account;
+use pyth_sdk_solana::state::{load_price_account, PriceAccount};
 
 use crate::errors::ObricError;
 
@@ -9,7 +9,10 @@ pub struct PriceFeed(pyth_sdk::PriceFeed);
 
 impl PriceFeed {
     pub fn price_normalized(&self, current_time: UnixTimestamp, age: u64) -> Result<Price> {
-        let p = self.0.get_price_no_older_than(current_time, age).ok_or(ObricError::PythOffline)?;
+        let p = self
+            .0
+            .get_price_no_older_than(current_time, age)
+            .ok_or(ObricError::PythOffline)?;
         let price = p.scale_to_exponent(-3).ok_or(ObricError::PythError)?;
         Ok(price)
     }
@@ -17,7 +20,8 @@ impl PriceFeed {
 
 impl AccountDeserialize for PriceFeed {
     fn try_deserialize_unchecked(data: &mut &[u8]) -> Result<Self> {
-        let account = load_price_account(data).map_err(|_x| error!(ObricError::PythError))?;
+        let account: PriceAccount =
+            load_price_account(data).map_err(|_x| error!(ObricError::PythError))?;
 
         // Use a dummy key since the key field will be removed from the SDK
         let zero = [0u8; 32];
